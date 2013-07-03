4e0f566feb22aa29dd059914a4039a1960d3d06c
v4l3nt1n <valentin.rusiechi@gmail.com>
2013-07-03 18:24:32 -0300

    Clase Formatter primera version con prueba

--- index.php
+++ index.php
@@ -3,47 +3,70 @@ ini_set('memory_limit', -1);
 
 include 'data.php';
 
-function sabre_cleaner($elem) {
-    return cleaner($elem, 'boleto', 'sabre');
+function cleaner($elem) {
+	global $cleaner_key;
+    return $elem[$cleaner_key];
 }
 
-function amadeus_cleaner($elem) {
-    return cleaner($elem, 'ticket', 'amadeus');
-}
-
-function backoffice_cleaner($elem) {
-    return cleaner($elem, 'tkt', 'backoffice');
-}
-
-function cleaner($elem, $key, $fuente) {
-    return $elem[$key];
-}
-
-function filterAmadeus($elem) {
+function filter($elem) {
+	global $filter_key;
     global $backoffice_clean_data;
-    return !in_array($elem['ticket'], $backoffice_clean_data);
+    return !in_array($elem[$filter_key], $backoffice_clean_data);
 }
 
-function filterSabre($elem) {
-    global $backoffice_clean_data;
-    return !in_array($elem['boleto'], $backoffice_clean_data);
+function classLoader ($pClassName) {
+    include(__DIR__ . "/" . $pClassName . ".php");
 }
+spl_autoload_register("classLoader");
 
 $start = microtime(true);
-// Se limpian datos de amadeus, sabre y bo
 
-$backoffice_clean_data = array_map('backoffice_cleaner', $backoffice);
+
+// ------------------------ OOP STYLAH BEGIN ------------------------------------------------
+/*
+$tickets = array(
+		'sabre' => $sabre,
+		'amadeus' => $amadeus,
+		'backoffice' => $backoffice,
+	);
+
+
+
+$Comparador = new Formatter($tickets);
+$faltantes = $Comparador->getMissing();
+/*
+echo "<pre>";
+print_r($faltantes);
+echo "</pre>";
+//*/
+
+// ------------------------ OOP STYLAH END ------------------------------------------------
+
+//*
+
+//========================== PROCEDURAL STYLAH BEGIN ========================================0
+// Se limpian datos del backoffice, ya que solo necesitamos los campos de sabre y amadeus
+$cleaner_key = 'tkt';
+$backoffice_clean_data = array_map('cleaner', $backoffice);
 // fin clean data
 
+$filter_key = 'ticket';
 // obtener faltantes amadeus
-$backoffice_faltantes_amadeus = array_filter($amadeus, 'filterAmadeus');
+$backoffice_faltantes_amadeus = array_filter($amadeus, 'filter');
 
+$filter_key = 'boleto';
 // obtener faltantes sabre
-$backoffice_faltantes_sabre = array_filter($sabre, 'filterSabre');
+$backoffice_faltantes_sabre = array_filter($sabre, 'filter');
+
+//========================== PROCEDURAL STYLAH END ========================================0
+//*/
 
 $end = microtime(true);
 
 echo ($end - $start) . ' seconds<hr />';
+
+
+
 
 //var_dump($backoffice_faltantes_amadeus);
 //var_dump($backoffice_faltantes_sabre);
